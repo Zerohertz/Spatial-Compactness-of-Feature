6190f79a5e52ca849debe030db2771ec1e2c5b7c
Zerohertz <ohg3417@gmail.com>
2021-02-19 09:52:24 +0900

    init

--- getNormFeatures.m
+++
@@ -1,9 +0,0 @@
-function normData = getNormFeatures(data, Winsize)
-for i = 1:fix(height(data) / Winsize)
-    for j = 1:width(data) - 1
-        normData(Winsize * (i - 1) + 1:Winsize * i, j) = normalize(data(Winsize * (i - 1) + 1:Winsize * i, j));
-    end
-end
-normData(1:height(normData), width(data)) = data(1:height(normData), width(data));
-end
-
--- scf.m
+++
@@ -1,9 +0,0 @@
-k = 1;
-for i = 1:6
-    for j = i + 1:7        
-        %NoSCF(k) = NumOfSCF(Normal(2).tab, Fault2(1).tab, [i, j], 20)
-        [NoSCF(k), Feat(k)] = NumOfSCFwithPlot(Normal(2).tab, Fault2(1).tab, [i, j], 20, '10000-10');
-        k = k + 1;
-        close
-    end
-end
